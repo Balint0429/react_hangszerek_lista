a7ea5a260e803a193bdd7b79d9fe0f02cd608bcb
Eniko <you@example.com>
2024-04-03 11:20:11 +0200

    nevjegy2

--- mobil/components/NevjegyScreen.js
+++ mobil/components/NevjegyScreen.js
@@ -6,7 +6,7 @@ const NevjegyScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Névjegy</Text>
       <Text style={styles.text}>Név: Laczkó Bálint</Text>
-      <Text style={styles.text}>Csoport: React Native</Text>
+      <Text style={styles.text}>Csoport: II/2/N</Text>
       <Text style={styles.text}>Aktuális dátum: 2024.04.03</Text>
     </View>
   );
